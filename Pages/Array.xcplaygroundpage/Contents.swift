5e62c070ecfc46e76e95c185ef879200ea38227e
shivam kumar <shivamkumar@shivams-MacBook-Pro.local>
2021-08-30 20:54:30 +0530

    Second Commit for Array

---
+++ Pages/Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+var emptyIntArr:[Int] = []
+print(emptyIntArr)
+let someIntArr = [1, 2, 3, 4, 5]
+let intArr = [10, 11, 12, 13, 14]
+print(intArr[0])
+print(intArr[1])
+print(intArr[2])
+print(intArr[3])
+print(intArr[4])
+emptyIntArr.insert(10, at: 0)
+for item in emptyIntArr {
+    print(item)
+}
