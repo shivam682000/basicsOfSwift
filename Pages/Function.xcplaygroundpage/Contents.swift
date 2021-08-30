34541ddb83a0e6382a90f799dca11d17524f1e2b
shivam kumar <shivamkumar@shivams-MacBook-Pro.local>
2021-08-30 23:24:24 +0530

    third commit for functions

---
+++ Pages/Function.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func wish(user:String) {
+    print("Good Morning \(user). Have a Good day.")
+}
+wish(user: "Shivam")
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
